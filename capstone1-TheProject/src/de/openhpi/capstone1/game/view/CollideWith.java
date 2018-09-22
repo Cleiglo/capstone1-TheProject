28ccef099794220a5e365ad220b3fee6e186bad5
Cleiglo <43414385+Cleiglo@users.noreply.github.com>
2018-09-22 21:37:53 +0200

    Add files via upload

--- capstone1-TheProject/src/de/openhpi/capstone1/game/view/CollideWith.java
+++ capstone1-TheProject/src/de/openhpi/capstone1/game/view/CollideWith.java
@@ -1,52 +1,91 @@
 package de.openhpi.capstone1.game.view;
 
-import de.openhpi.capstone1.game.starter.Settings;
 
-public class CollideWith {
+import javax.tools.Diagnostic;
+
+import de.openhpi.capstone1.game.starter.Settings;
+import processing.core.PApplet;
+
+/**
+ * The Class CollideWith.
+ */
+public class CollideWith extends AbstractView {
+
+	/** The ball speed. */
+	int[] ballSpeed = new int[2];
+
+	/**
+	 * Instantiates a new collide with.
+	 *
+	 * @param display the display
+	 */
 	
-	int ball_speed[] = new int[2];
-	
-	
-	public CollideWith(){
-		
-		
+	public CollideWith(final PApplet display) {
+		super(display);
 	}
-	
-	
-	
-	public boolean collideWithWall(int xball, int yball,int xspeed,int yspeed) {
+
+
+	/**
+	 * Collide with wall.
+	 *
+	 * @param xball  the xball
+	 * @param yball  the yball
+	 * @param xspeed the xspeed
+	 * @param yspeed the yspeed
+	 * @return true, if successful
+	 */
+	public boolean collideWithWall(final int xball, final int yball, int xspeed, int yspeed, int xpad) {
 		
-		if(xball+Settings.BALL_SIZE/2 == Settings.WINDOW_WIDTH) {
-			
+		int d =(int)Math.abs((yball + (int)Settings.BALL_SIZE / 2) -(int)(Settings.WINDOW_HEIGHT - Settings.PADDLE_HEIGHT)); 
+				
+		if ((xball + Settings.BALL_SIZE / 2 == Settings.WINDOW_WIDTH) || (xball - Settings.BALL_SIZE / 2 == 0)) {
 			xspeed = -xspeed;
-			ball_speed[0] = xspeed;
-			ball_speed[1] = yspeed;
-			
-			
-		}else if(xball-Settings.BALL_SIZE/2 == 0) {
-			
-			xspeed = -xspeed;
-			ball_speed[0] = xspeed;
-			ball_speed[1] = yspeed;
-			
-			
-		}else if(yball+Settings.BALL_SIZE/2 == Settings.WINDOW_HEIGHT) {
-//			ball collided with bottom edge of the screen?
+			ballSpeed[0] = xspeed;
+			ballSpeed[1] = yspeed;
+		} else if (yball + Settings.BALL_SIZE / 2 == Settings.WINDOW_HEIGHT) {
+			// ball collided with bottom edge of the screen?
 			System.out.println("Game Over");
-			ball_speed[0] = 0;
-			ball_speed[1] = 0;
+			ballSpeed[0] = 0;
+			ballSpeed[1] = 0;
+
 			return true;
-		}else if(yball-Settings.BALL_SIZE/2 == 0) {
-			
+		} else if (yball - Settings.BALL_SIZE / 2 == 0) {
 			yspeed = -yspeed;
-			ball_speed[0] = xspeed;
-			ball_speed[1] = yspeed;
+			ballSpeed[0] = xspeed;
+			ballSpeed[1] = yspeed;
+		} else if(d < 5) {
+			if(xball + (int)Settings.BALL_SIZE >= xpad && xball + (int)Settings.BALL_SIZE <= xpad +(int)Settings.PADDLE_WIDTH + 30) {
 			
-		}else {
-			ball_speed[0] = xspeed;
-			ball_speed[1] = yspeed;
+				yspeed = -yspeed;
+				if(xpad>= Settings.WINDOW_WIDTH) {
+					
+					xspeed = xspeed + 1;
+					
+				}else {
+					
+					xspeed = xspeed - 1;
+					
+				}
+				
+				ballSpeed[0] = xspeed;
+				ballSpeed[1] = yspeed;
+				
+			}
+		}else{
+			ballSpeed[0] = xspeed;
+			ballSpeed[1] = yspeed;
 		}
-		
+
 		return false;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 *
+	 * @see de.openhpi.capstone1.game.view.AbstractView#update()
+	 */
+	@Override
+	public void update() {
+
 	}
 }
